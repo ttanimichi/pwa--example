97f69a7660bf42cbbe665a681c2cff0516dee69a
Tsukuru Tanimichi <info+git@ttanimichi.com>
2017-10-27 16:27:09 +0900

    Initial commit

---
+++ sw.js
@@ -0,0 +1,35 @@
+var CACHE_NAME = 'my-site-cache-v1';
+
+var urlsToCache = [
+    '/',
+    '/style.css'
+];
+
+self.addEventListener('install', function(event) {
+    // インストール処理
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                // キャッシュがあったのでそのレスポンスを返す
+                if (response) {
+                    console.log('キャッシュあった');
+                    return response;
+                } else {
+                    console.log('キャッシュなかった');
+                }
+
+                return fetch(event.request);
+            })
+    );
+});
